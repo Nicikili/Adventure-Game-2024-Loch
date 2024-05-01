7b5b9ade9d02def98de965a79e03ef3e41d1cd27
Mono_o <gamma.marcel@gmail.com>
2024-05-01 14:41:34 +0200

    changes in PlayerController

--- AdventureGame_Loch_2024/Assets/Scripts/PlayerController.cs
+++ AdventureGame_Loch_2024/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    InputSystem controls;
+
+    private float playerSpeed = 20f; //Player Speed
+    private float playerDirection; //keeps track of the direction we are going
+    private Rigidbody2D rbPlayer; //is the Rigidbody of our Player
+
+    void Awake()
+    {
+		#region Input System
+		controls = new InputSystem();
+
+		controls.Player.Enable();
+
+		controls.Player.Jump.performed += ctx => Jump();
+        controls.Player.Accept.performed += ctx => Accept();
+        controls.Player.Move.performed += ctx => Move();
+		#endregion
+
+		rbPlayer = GetComponent<Rigidbody2D>(); //Assign Rigidbody Component
+
+    }
+
+    #region Player Controls
+
+    void Jump()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+	void Accept()
+	{
+
+	}
+	void Move()
+	{
+
+	}
+
+	#endregion
+
+	void OnEnable()
+	{
+        controls.Player.Enable();
+	}
+    
+    void OnDisable()
     {
-        
+        controls.Player.Disable();
     }
 }
