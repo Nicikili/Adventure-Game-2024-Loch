c4b3b11f0ee4aca07da7c65456a84d89c69bfaa3
Mono_o <gamma.marcel@gmail.com>
2024-05-02 16:50:50 +0200

    Serialize Field
    Added Serialize Field zu CameraFollow Script and changed Layers of the Prefab

--- AdventureGame_Loch_2024/Assets/Scripts/CameraFollow.cs
+++ AdventureGame_Loch_2024/Assets/Scripts/CameraFollow.cs
@@ -6,9 +6,13 @@ public class CameraFollow : MonoBehaviour
 {
 	public Transform player;
 
+	[SerializeField] int xAxis = 0; //CameraFollow for x Axis
+	[SerializeField] int yAxis = 0; // Camera Follow for y Axis
+	[SerializeField] int zAxis = 0; // Camera Follow for z Axis
+
 	// Update is called once per frame
 	void Update()
 	{
-		transform.position = player.transform.position + new Vector3(0, 6, 10);
+		transform.position = player.transform.position + new Vector3(xAxis, yAxis, zAxis);
 	}
 }
