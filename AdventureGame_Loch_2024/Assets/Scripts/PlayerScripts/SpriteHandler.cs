ac984d5a044453f47e92aca8ca7cbe3b9e1475cd
Mono_o <gamma.marcel@gmail.com>
2024-06-05 13:06:49 +0200

    BodyParts Change Player Behavoir
    Still needs some adjustement

--- AdventureGame_Loch_2024/Assets/Scripts/PlayerScripts/SpriteHandler.cs
+++ AdventureGame_Loch_2024/Assets/Scripts/PlayerScripts/SpriteHandler.cs
@@ -7,7 +7,6 @@ using UnityEditor.ShaderKeywordFilter;
 using UnityEngine;
 using UnityEngine.U2D.Animation;
 
-
 public class SpriteHandler : MonoBehaviour
 {
 	public GameObject findSwitchTargetIn;
@@ -15,13 +14,19 @@ public class SpriteHandler : MonoBehaviour
 
 	public GameObject IK_BonesTarget;
 	public string spriteNameBodyPart;
+	public bool canStealPart = false;
 
 	public PlayerStats ScriptStats;
 
 	public void Start()
 	{
+		//Important note, if you want to change Variables, add the base value here.
+		//This Script will make changes on the Prefab that stay otherwise.
 		ScriptStats.JumpPower = 0;
 		ScriptStats.GroundBaseSpeed = 20;
+		ScriptStats.ExtraConstantGravity = 120;
+
+		ScriptStats.MaxAirJumps = 0;
 	}
 
 	public void OnTriggerEnter2D(Collider2D other)
@@ -39,14 +44,28 @@ public class SpriteHandler : MonoBehaviour
 
 			spriteNameBodyPart = other.GetComponent<SpriteRenderer>().sprite.name;
 
-			if (spriteNameBodyPart == "S_Wing2")
-			{
-				ScriptStats.JumpPower = 80; 
-			}
-
 			if (spriteNameBodyPart == "S_Wing1")
 			{
+				ScriptStats.JumpPower = 80;
+				ScriptStats.GroundBaseSpeed = 30;
+			}
 
+			if (spriteNameBodyPart == "S_Wing2")
+			{
+				ScriptStats.JumpPower = 120;
+				ScriptStats.GroundBaseSpeed = 40;
+			}
+
+			if (spriteNameBodyPart == "S_Wing3")
+			{
+				ScriptStats.JumpPower = 100;
+				ScriptStats.GroundBaseSpeed = 50;
+				ScriptStats.ExtraConstantGravity = 0;
+			}
+
+			if (spriteNameBodyPart == "Tongue1"|| spriteNameBodyPart == "Tongue2")
+			{
+				canStealPart = true;
 			}
 		}
 	}
